837e1f3fc6493be6d76bb1381d756352f4eefdf3
web-des-project <sieversfrancesca7@gmail.com>
2023-11-03 13:18:55 -0300

    Prep 3 changed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 50);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
+
+    }   
 }
