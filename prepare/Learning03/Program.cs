0e7374442d358626e72a3c332c7fb995851a4295
web-des-project <sieversfrancesca7@gmail.com>
2023-11-14 19:00:36 -0300

    Learning03 updated

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number1 = new Fraction();
+        Console.WriteLine(number1.GetFractionString());
+        Console.WriteLine(number1.GetDecimalValue());
+
+        Fraction number2 = new Fraction(5);
+        Console.WriteLine(number2.GetFractionString());
+        Console.WriteLine(number2.GetDecimalValue());
+
+        Fraction number3 = new Fraction(3, 4);
+        Console.WriteLine(number3.GetFractionString());
+        Console.WriteLine(number3.GetDecimalValue());
+
+        Fraction number4 = new Fraction(1, 3);
+        Console.WriteLine(number4.GetFractionString());
+        Console.WriteLine(number4.GetDecimalValue());
     }
 }
