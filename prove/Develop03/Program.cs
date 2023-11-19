896e1bac1348cd6279f7321122b39175de8141bd
web-des-project <sieversfrancesca7@gmail.com>
2023-11-18 22:43:14 -0300

    Scripture fixed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,7 +7,7 @@ class Program
         Reference reference = new("Moroni", 10, 4, 5);
         Console.WriteLine();
 
-        string text = "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with dreal intent, having efaith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may aknow the btruth of all things.";
+        string text = "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may know the truth of all things.";
         Scripture scripture = new(text,reference);
         
 
